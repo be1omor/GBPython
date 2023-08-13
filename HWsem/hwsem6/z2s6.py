70ce8c28066ce6b3474937bec07978b044b7c87f
Ilya Lyalin <lyalin315@gmail.com>
2023-08-13 19:05:04 +0300

    add z2 v4

--- HWsem/hwsem6/z2s6.py
+++ HWsem/hwsem6/z2s6.py
@@ -9,14 +9,17 @@
 # Вывод:
 # 1, 3
 
-# list_1 = [3, 4, 2, 5, 7]
+# Вариант 1 (с вводом чисел с клавиатуры).
 min = 4
 max = 6
-list_1 = [int(i) for i in input().split()] # if min <= int(i) <= max]
+list_1 = [index for index, value in enumerate(input().split()) if min <= int(value) <= max]
+print(list_1)
 
-ind = []
-for i in range(len(list_1)):
-    if min <= list_1[i] <= max:
-        ind.append(i)
-print(*ind)
-# print(list_1)
+# Вариант 2.
+# list_1 = [3, 4, 2, 5, 7]
+# list_1 = [int(i) for i in input().split()]
+# ind = []
+# for i in range(len(list_1)):
+#     if min <= list_1[i] <= max:
+#         ind.append(i)
+# print(*ind)
